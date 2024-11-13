adca3fcb0aaa56dc5cec9333412a59940f631485
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-11-13 22:20:57 +0200

    app component logic changed to test-demo component

--- Components/demo-app/src/app/app.component.ts
+++ Components/demo-app/src/app/app.component.ts
@@ -3,33 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-list/user-item/user-item.component';
-import { beaverImgSrc } from './constants';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavigationBarComponent,
-    FooterComponent,
-    UserListComponent,
-  ],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'demo-app';
-
-  numbers = [1, 2, 3, 4, 5];
-
-  randomMsg = 'random-red';
-  isShown = true;
-  beaverImage = beaverImgSrc;
-
-  showForm(event: Event){
-    console.log(event); 
-    this.randomMsg = this.randomMsg === 'random-green'? 'random-red' : 'random-green ';
-      this.isShown = !this.isShown
-  }
+  title = 'demo-app';  
 }
--- Components/demo-app/src/app/test-demo/test-demo.component.ts
+++ Components/demo-app/src/app/test-demo/test-demo.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { beaverImgSrc } from '../constants';
+import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { UserListComponent } from '../user-list/user-list.component';
 
 @Component({
   selector: 'app-test-demo',
   standalone: true,
-  imports: [],
+  imports: [ 
+    NavigationBarComponent,
+    FooterComponent,
+    UserListComponent,
+  ],
   templateUrl: './test-demo.component.html',
   styleUrl: './test-demo.component.css'
 })
 export class TestDemoComponent {
+  numbers = [1, 2, 3, 4, 5];
 
+  randomMsg = 'random-red';
+  isShown = true;
+  beaverImage = beaverImgSrc;
+
+  showForm(event: Event) {
+    console.log(event);
+    this.randomMsg = this.randomMsg === 'random-green' ? 'random-red' : 'random-green ';
+    this.isShown = !this.isShown
+  }
 }
