35377accde4453e08c7655a140c2440deec22102
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-11-27 22:20:41 +0200

    self implemented interval method from rxjs

--- RxJS and Services/demo-app/src/promise-vs-observables.ts
+++ RxJS and Services/demo-app/src/promise-vs-observables.ts
@@ -1,8 +1,49 @@
-const p = new Promise((resolve, reject) =>{
-    console.log('A promise has been initiated');
-    setTimeout(() =>{
-      resolve(1000); 
-    }, 3000);
-  });
+// import { Observable } from "rxjs";
 
-  p.then(data => console.log(data));
+import { interval, Observable } from "rxjs"
+
+// //Promises - not lazy (invoking without getting to the result )
+// const p = new Promise((resolve, reject) => {
+//   console.log('A promise has been initiated');
+//   setTimeout(() => {
+//     resolve(1000);
+//   }, 3000);
+// });
+
+// p.then(data => console.log('Promise: ', data));
+
+// //Observables - lazy (not invoking when not subscribed)
+// const o = new Observable((observer) => {
+//   setTimeout(() => {
+//     observer.next(101);
+//     observer.next(102);
+//     observer.next(103);
+//   }, 2000)
+
+// });
+
+// o.subscribe((data) => {
+//   console.log('from observable: ', data);
+// })
+
+
+// const interval = (intervalValue: number) => {
+//   const o = new Observable<number>((observer) => {
+//     let counter = 0;
+
+//     const timer = setInterval(() => {
+//       observer.next(counter++);
+//     }, intervalValue);
+
+//     return () => {
+//       clearInterval(timer);
+//     }
+//   });
+
+//   return o;
+// }
+
+interval(2000)
+  .subscribe((data) => {
+    console.log(data);
+  }); 
