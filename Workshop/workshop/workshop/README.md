b0a16511154e4c4fc557d832eb1b6cb3e6d1f914
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-11-28 21:17:43 +0200

    new project created

--- Workshop/workshop/workshop/README.md
+++ Workshop/workshop/workshop/README.md
@@ -1,4 +1,4 @@
-# PostBlog
+# Workshop
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.11.
 
--- Workshop/workshop/workshop/src/app/app.component.spec.ts
+++ Workshop/workshop/workshop/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'post-blog' title`, () => {
+  it(`should have the 'workshop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('post-blog');
+    expect(app.title).toEqual('workshop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, post-blog');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, workshop');
   });
 });
