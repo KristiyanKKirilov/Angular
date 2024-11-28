34151f5b691e5b6468421154037e36b0e9bd0680
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-11-28 19:31:04 +0200

    configuring environment settings

---
+++ Workshop/post-blog/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDYz4He70tCEMrWAGa62rxbdZAKU2wHTbU",
+        authDomain: "post-blog-ng.firebaseapp.com",
+        databaseURL: "https://post-blog-ng-default-rtdb.firebaseio.com",
+        projectId: "post-blog-ng",
+        storageBucket: "post-blog-ng.firebasestorage.app",
+        messagingSenderId: "297123597302",
+        appId: "1:297123597302:web:0e6cb14df816fed0a6d5d7",
+        measurementId: "G-2HV2N357F1" 
+    }
+}
