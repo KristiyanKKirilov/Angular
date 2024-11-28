d7210d76abc16bea8f36202b19407a188d0b4b52
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-11-28 21:43:02 +0200

    initial structure created

--- Workshop/workshop/workshop/src/app/app.component.ts
+++ Workshop/workshop/workshop/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'workshop';
+export class AppComponent implements OnInit{
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://post-blog-ng-default-rtdb.firebaseio.com/posts/.json')
+    .subscribe((x) => {
+      console.log(x)
+    });
+  }
 }
