53c950ccd62c7b3e414abdf6830c51867d5da4d7
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-01 17:40:52 +0200

    loader implented

--- Workshop/workshop/workshop/src/app/themes-list/themes-list.component.ts
+++ Workshop/workshop/workshop/src/app/themes-list/themes-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Theme } from '../types/theme';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-themes-list',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './themes-list.component.html',
   styleUrl: './themes-list.component.css'
 })
 export class ThemesListComponent implements OnInit{
   themes: Theme[] = [];
+  isLoading = true;
 
   constructor(private apiService: ApiService){}
 
@@ -19,6 +21,7 @@ export class ThemesListComponent implements OnInit{
     .getThemes(5)
     .subscribe(themes => {
       this.themes = themes;
+        this.isLoading = false;
     });
   }
 
