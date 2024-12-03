3a651fb7179208f76a973edfabc020e3e2b4f82f
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-03 18:43:47 +0200

    authentication guard created

--- Modules-Signals-Routing/demo-app/src/app/app.routes.ts
+++ Modules-Signals-Routing/demo-app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailsComponent } from './users/user-details/user-details.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { AuthGuard } from './users/users.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,8 +11,11 @@ export const routes: Routes = [
     {
         path: 'users',
         children: [
-        { path: '', component: UsersComponent },
-        { path: 'details/:id', component: UserDetailsComponent }
+            { path: '', component: UsersComponent },
+            {
+                path: 'details/:id', component: UserDetailsComponent,
+                canActivate: [AuthGuard]
+            },
         ]
     },
     { path: '**', component: ErrorPageComponent },
