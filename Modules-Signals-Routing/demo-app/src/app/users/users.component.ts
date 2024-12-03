b71ec6f794b718ad17fc07101f4e19ef0fffd9f3
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-03 17:24:31 +0200

    nav button style changed

--- Modules-Signals-Routing/demo-app/src/app/users/users.component.ts
+++ Modules-Signals-Routing/demo-app/src/app/users/users.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
 import { User } from '../types/User';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
