2eccb3cad91d59bd2985cdcf2221aff3703533ad
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-04 11:40:43 +0200

    redirecting to error component on wrong url

--- Workshop/workshop/workshop/src/app/app.component.ts
+++ Workshop/workshop/workshop/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,6 @@ import { MainComponent } from './main/main.component';
     RouterOutlet,
     HeaderComponent,
     FooterComponent,
-    MainComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- Workshop/workshop/workshop/src/app/main/main.component.ts
+++ Workshop/workshop/workshop/src/app/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { PostsListComponent } from "../posts-list/posts-list.component";
+import { ThemesListComponent } from "../theme/themes-list/themes-list.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    PostsListComponent, 
+    ThemesListComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
