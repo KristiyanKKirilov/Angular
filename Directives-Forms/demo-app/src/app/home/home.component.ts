7356a901c95c1d91331e3eca8de4584a25f31748
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-06 21:09:40 +0200

    structural directives example

--- Directives-Forms/demo-app/src/app/home/home.component.ts
+++ Directives-Forms/demo-app/src/app/home/home.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { HighlightDirective } from '../directives/highlight.directive';
-import { RouterLink } from '@angular/router';
 import { MyRouterLinkDirective } from '../directives/my-router-link.directive';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HighlightDirective,
-    MyRouterLinkDirective
+    MyRouterLinkDirective,
+    CommonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent {  
+  randomPercentage = Math.floor(Math.random() * 100);
+  widthInPercentage = `${this.randomPercentage}%`;
+  isPercentageAbove50 = this.randomPercentage >= 50;
 
 }
