73ddf8d8ffe4f2b596f58d34de9414a83aabcea1
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-08 00:25:59 +0200

    invalid input email data error messages

--- Directives-Forms/demo-app/src/app/login/login.component.ts
+++ Directives-Forms/demo-app/src/app/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('loginForm') form: NgForm | undefined;
 
+  formSubmitHandler(): void {
+    console.log('From submitted');
+    console.log(this.form); 
+
+    if(this.form?.invalid){
+      console.log('Invalid form');
+      return;
+    }
+
+    console.log(this.form?.value);
+  }
 }
