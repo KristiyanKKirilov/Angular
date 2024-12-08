ffab2f59046d97bbc45fb72effe9d2c3be32673a
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-08 15:54:49 +0200

    login validation email completed

--- Workshop/workshop/workshop/src/app/user/login/login.component.ts
+++ Workshop/workshop/workshop/src/app/user/login/login.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { EmailDirective } from '../../directives/email.directive';
+import { DOMAINS } from '../../constants';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule,
+    EmailDirective,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  domains: string[] = DOMAINS;
+  
   constructor(
     private userService: UserService,
     private router: Router){}
 
-  login(e: Event, email: string, password: string): void{
-    e.preventDefault();
+  login(form: NgForm): void{
+    if(form.invalid){
+      return;
+    }
     this.userService.login();
     this.router.navigate(['/home']);
   }
