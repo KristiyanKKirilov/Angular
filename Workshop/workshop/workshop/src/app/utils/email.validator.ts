b7fa1e542f30e91ba6a8b9071a932b740aa18ce4
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-09 14:10:27 +0200

    email validator completed

--- Workshop/workshop/workshop/src/app/utils/email.validator.ts
+++ Workshop/workshop/workshop/src/app/utils/email.validator.ts
@@ -3,7 +3,6 @@ import { ValidatorFn } from "@angular/forms";
 export function emailValidator(domains: string[]):ValidatorFn{
     const domainString = domains.join('|');
     const regExp = new RegExp(`[A-Za-z0-9._ ]{6,}@gmail.(${domainString})`);
-    console.log({regExp});
 
     return (control) => {
         const isInvalid = control.value === '' || regExp.test(control.value);
