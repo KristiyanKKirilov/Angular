79a2f48d06212570400d01bd1fc9ff8eaad3db72
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-12-11 00:08:02 +0200

    pipes example

--- Pipes-Interceptors-Subjects/demo-app/src/app/app.component.ts
+++ Pipes-Interceptors-Subjects/demo-app/src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule, LowerCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-app';
+
+
+  user = {
+    name: 'John',
+    age: 22,
+    grades: [5, 4, 2, 3, 4]
+  };
+
+  addProperty() {
+    (this.user as any).test = 'test123';
+    this.user.grades.push(-5);
+  }
+
+
+  p = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1000);
+    }, 2000);
+  });
+
+  time$ = interval(1000).pipe(map(() => new Date()));
 }
